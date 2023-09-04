53e4b683e2bf762c51e9c8f58491de5a14f5081c
Dashhyphenminus <robertblendea@outlook.com>
2023-09-04 11:15:04 -0700

    add Utility

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+    public HelloWorld (String[] args) {
+        
+    }
+    public void print () {
+        System.out.println("Hello, World!");
     }
 }
---
+++ Utility.java
@@ -0,0 +1,6 @@
+public class Utility {
+    public static void main (String[] args) {
+        HelloWorld thing = new HelloWorld(null);
+        thing.print();
+    }
+}
